c1432223fad8a19a1d0ea80ecf794e3fbddcc9b9
Jens Frey <jens.frey@coffeecrew.org>
2020-10-03 23:18:25 +0200

    Change site

--- publishconf.py
+++ publishconf.py
@@ -10,9 +10,9 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://authsec.github.io/action-blog-test'
+SITEURL = 'https://authsec.github.io/datapile'
 RELATIVE_URLS = False
-THEME = '/pelican-themes/flex'
+#THEME = '/pelican-themes/flex'
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
